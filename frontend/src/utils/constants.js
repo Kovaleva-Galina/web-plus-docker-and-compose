66dbff2e9a1bc513a8a42e345fee31ecda073f45
Kovaleva-Galina <galkina.galina.alexandrovna@yandex.ru>
2023-12-26 22:07:48 +0300

    fix: small fixes

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://127.0.0.1:4000";
+export const URL = process.env.REACT_APP_BACKEND_URL;
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
