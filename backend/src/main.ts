05714815df0eff1db3442a079a798610f24c69a1
Kovaleva-Galina <galkina.galina.alexandrovna@yandex.ru>
2023-12-27 10:11:24 +0300

    fix: small fixes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,9 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: 'https://galina.kovaleva.kupi.nomoredomainsmonster.ru',
-    },
+    cors: true,
     bufferLogs: true,
   });
   const config = app.get(ConfigService);
